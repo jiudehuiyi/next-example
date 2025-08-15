538de2de6ec863bb708ee3aa14f8905877c33cc0
jiudehuiyi <136859304@qq.com>
2025-08-15 10:26:30 +0800

    feat

--- app/pages/page.tsx
+++ app/pages/page.tsx
@@ -1,20 +1,9 @@
 'use client';
-
-import { useState } from "react";
-import About from "./about";
-import Home from "./index";
-import Head from "next/head";
-import { usePathname, useSearchParams } from "next/navigation";
-
 const Index = () => {
-    const pathname = usePathname();
-    const searchParams = useSearchParams();
-    const [count, setCount] = useState(1);
-    console.log('pathnamepathname', pathname, searchParams, searchParams.get("aa") );
+ 
     return (
         <div>
-            Page-{count}
-            <button onClick={ () => setCount(count + 1) }>Button</button>
+            AAA
         </div>
     )
 }
